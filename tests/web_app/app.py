b5d16fb2227a207e29683f7b96b566da65578fc8
Nikita Paramonov <nparamonov27@gmail.com>
2023-04-03 00:11:29 +0300

    [tests] Scrolling down an infinite page

--- tests/web_app/app.py
+++ tests/web_app/app.py
@@ -12,5 +12,13 @@ def ping():
     return responses.PlainTextResponse('pong')
 
 
+@app.get('/infinite_page')
+def infinite_page():
+    """ Returns a page that adds content when scrolling down """
+    with open('templates/infinite_page.html') as html_file:
+        html_source = html_file.read()
+    return responses.HTMLResponse(html_source)
+
+
 if __name__ == '__main__':
     uvicorn.run('app:app', host='localhost', port=8000, reload=True)
