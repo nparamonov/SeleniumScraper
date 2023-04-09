00c9c6fac580cfe71e506ed0235b75f762562950
Nikita Paramonov <nparamonov27@gmail.com>
2023-04-09 18:24:43 +0300

    Fixed scrolling, added several methods

---
+++ selenium_scraper/mapping.py
@@ -0,0 +1,5 @@
+class ScrollMethods:
+    """ Available ways to scroll the page down """
+    js_instant = 'js_instant'
+    js_smooth = 'js_smooth'
+    end_key = 'end_key'
--- tests/test_scroll.py
+++ tests/test_scroll.py
@@ -1,10 +1,44 @@
+import pytest
+from selenium_scraper.mapping import ScrollMethods
 
-def test_scroll_infinite_page(scraper, base_url):
-    """ Checks for scrolling down an infinite page """
+
+def test_scroll_infinite_page_end_key(scraper, base_url):
+    """ Checks for scrolling down an infinite page with `end_key` method """
     n_scrolls = 5
 
     scraper.get(base_url + '/infinite_page')
-    scraper.scroll_down(n_scrolls, 2)
+    scraper.scroll_infinite_page(n_scrolls, 2, ScrollMethods.end_key)
 
     paragraphs = scraper.current_page.find_all('div', {'class': 'paragraph'})
     assert len(paragraphs) - 1 == n_scrolls
+
+
+def test_scroll_infinite_page_js_instant(scraper, base_url):
+    """ Checks for scrolling down an infinite page with `js_instant` method """
+    n_scrolls = 5
+
+    scraper.get(base_url + '/infinite_page')
+    scraper.scroll_infinite_page(n_scrolls, 2, ScrollMethods.js_instant)
+
+    paragraphs = scraper.current_page.find_all('div', {'class': 'paragraph'})
+    assert len(paragraphs) - 1 == n_scrolls
+
+
+def test_scroll_infinite_page_js_smooth(scraper, base_url):
+    """ Checks for scrolling down an infinite page with `js_smooth` method """
+    n_scrolls = 5
+
+    scraper.get(base_url + '/infinite_page')
+    scraper.scroll_infinite_page(n_scrolls, 2, ScrollMethods.js_smooth)
+
+    paragraphs = scraper.current_page.find_all('div', {'class': 'paragraph'})
+    assert len(paragraphs) - 1 == n_scrolls
+
+
+def test_scroll_infinite_page_wrong_method(scraper, base_url):
+    """ Checks for scrolling down an infinite page with wrong method """
+    n_scrolls = 5
+
+    scraper.get(base_url + '/infinite_page')
+    with pytest.raises(ValueError):
+        scraper.scroll_infinite_page(n_scrolls, 2, 'my_method')
