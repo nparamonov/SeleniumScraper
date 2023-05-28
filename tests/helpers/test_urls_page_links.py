76d9dca947bf82b6ce51296427ebbbcc9695ead4
Nikita Paramonov <nparamonov27@gmail.com>
2023-05-28 19:04:47 +0300

    [tests] Add tests for selenium_scraper.helpers.urls

---
+++ tests/helpers/test_urls_page_links.py
@@ -0,0 +1,16 @@
+from selenium_scraper.helpers.urls import PageLinks
+
+LINKS = ['/', '/', '/about', 'page3', 'javascript:void(0)', 'https://github.com/nparamonov/SeleniumScraper',
+         '//github.com/nparamonov']
+
+
+def test_links():
+    """ Check the processing of various urls """
+    page_links = PageLinks('https://example.com/page1/page2')
+    for link in LINKS:
+        page_links.add_link(link)
+
+    assert page_links.external == {'https://github.com/nparamonov',
+                                   'https://github.com/nparamonov/SeleniumScraper'}
+    assert page_links.internal == {'https://example.com/page1/page3',
+                                   'https://example.com/about', 'https://example.com/'}
